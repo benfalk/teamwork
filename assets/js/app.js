5cef906b4ead17caadcf7128a9224cc93a6eaa1d
Ben Falk <benjamin.falk@yahoo.com>
2021-06-11 07:54:26 -0400

    Adding in AlpineJS

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import "../css/app.scss"
+import Alpine from "alpinejs";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -18,7 +19,16 @@ import topbar from "topbar"
 import {LiveSocket} from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket =
+    new LiveSocket("/live",
+                    Socket,
+                    { params: {_csrf_token: csrfToken},
+                      dom: {
+                        onBeforeElUpdated(from, to){
+                          if(from.__x){ Alpine.clone(from.__x, to) }
+                        }
+                    } }
+                  )
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
